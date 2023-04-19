60e6c26cd61bfc2fe3edbed758186cd81f5a3a7f
Visweswarji <123442277+visweswar5555955@users.noreply.github.com>
2023-04-19 21:16:47 +0530

    Update variables.tf
    updated

--- v2-VPC_with_EKS/variables.tf
+++ v2-VPC_with_EKS/variables.tf
@@ -1,27 +1,35 @@
 variable "location" {
-    default = "ap-south-1"
+default = "eu-central-1"
 }
-
 variable "os_name" {
-    default = "ami-09ba48996007c8b50"
-}
+default = "ami-09ba48996007c8b50"
 
+}
 variable "key" {
-    default = "rtp-03"
-}
+default = "rtp-03"
 
+}
 variable "instance-type" {
-    default = "t2.small"
+default = "t2.small"
 }
 
 variable "vpc-cidr" {
-    default = "10.10.0.0/16"  
+default = "10.10.0.0/16" 
 }
 
 variable "subnet1-cidr" {
-    default = "10.10.1.0/24"
-  
+default = "10.10.3.0/24"
+
 }
+variable "subnet2-cidr" {
+ default = "10.10.2.0/24"
+}
+
 variable "subent_az" {
-    default =  "ap-south-1a"  
+default = "eu-central-1a"
+
+}
+variable "subent-2_az" {
+default = "eu-central-1b" 
+
 }
